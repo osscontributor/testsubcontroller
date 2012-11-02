dcaf5017459f99acd005e29498762989490ea654
hbbg <bella.nuton@gmail.com>
2012-11-02 12:33:00 +0400

    Update test/unit/com/demo/MyControllerTests.groovy

--- test/unit/com/demo/MyControllerTests.groovy
+++ test/unit/com/demo/MyControllerTests.groovy
@@ -3,6 +3,27 @@ package com.demo
 import grails.test.*
 
 class MyControllerTests extends ControllerUnitTestCase {
+    
+    //    def grailsApplication
+
+    protected void setUp() {
+        super.setUp()
+
+        /*mockConfig '''
+            someURL = 'aa'
+        '''*/
+
+        /*def mockedConfig = new ConfigObject()
+        mockedConfig.someURL = "aa"
+        ConfigurationHolder.config = mockedConfig*/
+
+//        grailsApplication.config.someURL="aa"
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+    
     void testCreate() {
         controller.create()
         assert 'MyController.created' == controller.response.contentAsString
